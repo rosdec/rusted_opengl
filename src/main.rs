26150247879816467adaa6e61928d40efc590735
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2023-02-26 18:23:53 +0100

    *** ADD *** Added window

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_title("A fantastic window!")
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_wait();
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => control_flow.set_exit(),
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            _ => (),
+        }
+    });
+}
